b8616bf535f06935e7a61c6f658b969c35dc428b
melon_dxy <melon_dxy@126.com>
2023-08-22 17:31:04 +0800

    [Feature]分页

--- src/component/blogList.vue
+++ src/component/blogList.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="blog-list">
     <ul>
-      <li v-for="blog in blogs" :key="blog.id">
+      <li v-for="blog in showBlogs" :key="blog.id">
         <div class="blog-date">{{ blog.date }}</div>
         <div class="blog-title">{{ blog.title }}</div>
         <div class="blog-tags">
@@ -16,12 +16,54 @@
         </div>
       </li>
     </ul>
+    <el-pagination
+      small
+      layout="prev, pager, next"
+      :total="blogs.length"
+      :page-size="pageSize"
+      :currentPage="currentPage"
+      @prev-click="previousPage"
+      @next-click="nextPage"
+      @current-change="handleCurrentChange"
+    >
+    </el-pagination>
   </div>
 </template>
 
 <script>
 export default {
   props: ["blogs"],
+  data() {
+    return {
+      showBlogs: [],
+      currentPage: 1,
+      pageSize: 4,
+    };
+  },
+  watch: {
+    currentPage: function () {
+      let startIndex = (this.currentPage - 1) * this.pageSize;
+      this.showBlogs = this.blogs.slice(startIndex, startIndex + this.pageSize);
+    },
+  },
+  created() {
+    this.showBlogs = this.blogs.slice(0, this.pageSize);
+  },
+  methods: {
+    previousPage: function () {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage: function () {
+      if (this.currentPage < this.blogs.length) {
+        this.currentPage++;
+      }
+    },
+    handleCurrentChange: function(val){
+        this.currentPage = val;
+    }
+  },
 };
 </script>
 
